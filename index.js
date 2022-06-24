e8925b440a0dfd93d36dc2c8d12d0dbb2b185e25
Moe-Zart <mohamedzreika03@outlook.com>
2022-06-24 20:18:57 +1000

    basics of promises

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ setTimeout(() => console.log("We are in the timeout"), 5000);
 
 console.log("End");
 */
-
+/*
 console.log("Start");
 
 function loginUser(email, password, callback) {
@@ -23,11 +23,60 @@ function getUserVideos(email, callback) {
     callback(["vid1", "vid2", "vid3"]);
   }, 2000);
 }
+
+function getVideoDetails(videos, callback) {
+  setTimeout(() => {
+    console.log("Here's your details");
+    callback(["The Don", "Beastly Trials", "Techtical"]);
+  }, 2000);
+}
 const user = loginUser("feshoisf@gmail.com", 123456, (user) => {
   console.log(user);
-  getUserVideos(user.userEmail, vids =>{
+  getUserVideos(user.userEmail, (vids) => {
     console.log(vids);
-  })
+    getVideoDetails(vids[1], (BT) => {
+      console.log(BT);
+    });
+  });
 });
 
 console.log("Finish");
+*/
+
+//promise is an object that gives us back a result of an asynchronous operation or a failure of it.
+/*
+const promise = new Promise((resolve, reject) =>{
+  setTimeout(()=>{
+    resolve({user: "ed"})
+  },2000);
+})
+promise.then(user => {
+  console.log(user);
+})
+*/
+
+console.log("person 1: shows ticket");
+console.log("person 2: shows ticket");
+//i am person 3 and i dont have ticket, but i make a promise to the ticketer that my
+// friend is coming with the ticket
+const promiseFriendBringsTicket = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("ticket"); //receiving the ticket in 3 seconds
+  }, 3000);
+});
+
+//.then means once the promise is fulfilled
+// we make the next promise equal to the .then so that it runs after the previous promise is fulfilled
+const getPopcorn = promiseFriendBringsTicket.then((t) => {
+  console.log(t);
+  console.log('"lets go get popcorn"');
+  return new Promise((resolve, reject) => resolve(t + ", popcorn"));
+});
+const getButter = getPopcorn.then((t) => {
+  console.log(t);
+  console.log('"we need butter"');
+  return new Promise((resolve, reject) => resolve(t + ", butter"));
+});
+getButter.then((t) => console.log(t));
+console.log("person 4: shows ticket");
+console.log("person 5: shows ticket");
